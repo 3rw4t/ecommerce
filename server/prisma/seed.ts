2feb98d19e7fb6b2ed4c83a9a79788f32575f5b7
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-09-24 16:29:27 -0300

    Dockerize db seeding + moved client prisma folder
    Now seeding works with Docker and generated folder is created in prisma folder

---
+++ server/prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "./generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.Product.createMany({
+        data: [
+            {
+            name: "White Shirt",
+            description: "Size M, Full White",
+            price: 59.99,
+            stock: 100,
+            sale_percentaje: 0.00,},
+            {
+            name: "Red Shirt",
+            description: "Size M, Full Red",
+            price: 59.99,
+            stock: 100,
+            sale_percentaje: 0.00,},
+        ],
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
